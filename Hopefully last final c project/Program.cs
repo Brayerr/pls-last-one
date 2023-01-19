6c214edfb5deb56dfd0d98aa9908105f334eff0a
Brayerr <brayer_alon@walla.com>
2023-01-19 19:38:00 +0200

    s

---
+++ Hopefully last final c project/Program.cs
@@ -0,0 +1,11 @@
+﻿using Engine_Final_Project;
+Vector2 pos = new Vector2(1, 1);
+RectangleTile recTile = new RectangleTile(pos);
+Tilemap map = new Tilemap(recTile, 8, 8);
+map.GetEnumerator();
+Console.WriteLine($"This map has {map.Count()} items.");
+
+//foreach (var item in map)
+//{
+//    Console.WriteLine();
+//}
---
+++ Hopefully last final c project/Tile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine_Final_Project
+{
+    public abstract class Tile
+    {
+        public Vector2 position { get; protected set; }
+        public int indexer { get; protected set; }
+        public enum Actor
+        {
+            Player1,
+            Player2
+        }
+        public Actor actor;
+
+        public void IndexerSetter(int value)
+        {
+            indexer = value;
+        }
+    }
+}
